407fa6e514d31e913c797ed5e0c9333e370d4ffe
yangyue <yy@moz0.com>
2018-12-13 15:51:28 +0800

    fix reset

--- packages/admin-header/src/admin-header.vue
+++ packages/admin-header/src/admin-header.vue
@@ -12,7 +12,7 @@
                 class="el-admin-header--item"
                 :class="{'is-active':item.sourcesCode === active}"
                 :key="item.sourcesCode" 
-                @click="clickHandler(item.sourcesCode)"
+                @click="$emit('update:active', item.sourcesCode)"
             >
                 <icon class="el-admin-header--icon" :name="item.sourcesCode"></icon>
                 <div class="el-admin-header--name">{{item.sourcesName}}</div>
@@ -31,21 +31,6 @@ export default {
       required: true
     },
     active: String
-  },
-
-  created() {
-    this.setDefaultActive();
-  },
-
-  methods: {
-    clickHandler(value) {
-      this.$emit('update:active', value);
-    },
-    setDefaultActive() {
-      if (!this.active && this.sources[0]) {
-        this.$emit('update:active', this.sources[0].sourcesCode);
-      }
-    }
   }
 };
 
